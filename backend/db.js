90bf6824645c99fdfed8fb67603bf42946dd3017
vatsal <vatsalmunjpara3@gmail.com>
2025-05-07 13:08:29 +0530

    add origin main

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,23 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config({
+    origin: 'https://task-tracker-app-theta.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+});
 
 
-const connectDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_URI,{
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
         console.log("Database connected")
     }
-    catch(err){
-        console.log("mongodb connection error",err)
+    catch (err) {
+        console.log("mongodb connection error", err)
         process.exit(1);
     }
 }
